0c3156b3ec12b09f69c36d43e7c33e09cc6189f0
BLGALEX <lepehaleha@bk.ru>
2020-03-04 22:22:55 +1000

    Теперь выделение массивов динамическое и для трёх кнопок достаточно одной функции

--- LSD-introsort_compare/Source.cpp
+++ LSD-introsort_compare/Source.cpp
@@ -61,12 +61,13 @@ int compare(const void*a, const void* b)
 	if (*(check_stable*)a > *(check_stable*)b) return 1;
 }
 
-void ten_random_numbers()
+void random_numbers(int N = 10)
 {
-	const int N = 10;
 	srand(time(NULL));
-	int LSD_array_main[N], Introsort_array_main[N], Introsort_array_check[N];
-	check_stable Introsort_array_stable[N];
+	int *LSD_array_main = new int[N];
+	int *Introsort_array_main = new int[N];
+	int *Introsort_array_check= new int[N];
+	check_stable* Introsort_array_stable = new check_stable[N];
 	int x;
 	for (int i = 0; i < N; i++)
 	{
@@ -94,71 +95,6 @@ void ten_random_numbers()
 	system("cls");
 }
 
-void thouthand_random_numbers()
-{
-	const int N = 1000;
-	srand(time(NULL));
-	int LSD_array_main[N], Introsort_array_main[N], Introsort_array_check[N];
-	check_stable Introsort_array_stable[N];
-	int x;
-	for (int i = 0; i < N; i++)
-	{
-		x = rand();
-		LSD_array_main[i] = x;
-		Introsort_array_main[i] = x;
-		Introsort_array_check[i] = x;
-		Introsort_array_stable[i].key = x;
-		Introsort_array_stable[i].check = i;
-	}
-	int LSD_time1, LSD_time2, Introsort_time1, Introsort_time2;
-	LSD_time1 = clock();
-	LSDSort(LSD_array_main, LSD_array_main + N - 1);
-	LSD_time2 = clock();
-	Introsort_time1 = clock();
-	Introsort(Introsort_array_main, N);
-	Introsort_time2 = clock();
-	system("cls");
-	std::cout << "LSDsort time: ";
-	std::cout << LSD_time2 - LSD_time2 << std::endl;
-	std::cout << "Introsort time ";
-	std::cout << Introsort_time2 - Introsort_time1 << std::endl;
-	std::cout << "Press any key to continue ";
-	_getch();
-	system("cls");
-}
-
-void houndred_thouthands_random_numbers()
-{
-	const int N = 100000;
-	srand(time(NULL));
-	int LSD_array_main[N], Introsort_array_main[N], Introsort_array_check[N];
-	check_stable Introsort_array_stable[N];
-	int x;
-	for (int i = 0; i < N; i++)
-	{
-		x = rand();
-		LSD_array_main[i] = x;
-		Introsort_array_main[i] = x;
-		Introsort_array_check[i] = x;
-		Introsort_array_stable[i].key = x;
-		Introsort_array_stable[i].check = i;
-	}
-	int LSD_time1, LSD_time2, Introsort_time1, Introsort_time2;
-	LSD_time1 = clock();
-	LSDSort(LSD_array_main, LSD_array_main + N - 1);
-	LSD_time2 = clock();
-	Introsort_time1 = clock();
-	Introsort(Introsort_array_main, N);
-	Introsort_time2 = clock();
-	system("cls");
-	std::cout << "LSDsort time: ";
-	std::cout << LSD_time2 - LSD_time2 << std::endl;
-	std::cout << "Introsort time ";
-	std::cout << Introsort_time2 - Introsort_time1 << std::endl;
-	std::cout << "Press any key to continue ";
-	_getch();
-	system("cls");
-}
 
 int main()
 {
@@ -175,9 +111,9 @@ int main()
 	{
 		switch (get_key)
 		{
-		case'1': ten_random_numbers(); break;
-		case'2': thouthand_random_numbers(); break;
-		case'3': houndred_thouthands_random_numbers(); break;
+		case'1': random_numbers(10); break;
+		case'2': random_numbers(1000); break;
+		case'3': random_numbers(100000); break;
 		case'4': break;
 		default: std::cout << "Wrong key, press any key to continue"; get_key = _getch(); system("cls"); break;
 		}
