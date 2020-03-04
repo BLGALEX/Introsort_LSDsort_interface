f848132d6583769c1118627ec70aac47649de708
BLGALEX <lepehaleha@bk.ru>
2020-03-04 20:51:21 +1000

    Добавлены комментарии в LSD-sort

--- LSD-introsort_compare/LSD_sort.h
+++ LSD-introsort_compare/LSD_sort.h
@@ -1,6 +1,16 @@
 #ifndef LSD_sort
 #define LSD_sort
 
+/**
+ * input: положительное число и номер нужного разряда (счёт с младших 1, 2, 3...)
+ *
+ * output: возварщает цифру, стоящую на месте указанного разряда
+ *
+ * Функция нужна для получения значения указанного разряда из кого-либо числа
+ *
+ * @param number - положительное целое число 
+ * @param digit - номер разряда 
+ */
 int Getdigit(unsigned int number, int digit)
 {
 	for (digit; digit > 1; digit--)
@@ -11,11 +21,26 @@ int Getdigit(unsigned int number, int digit)
 	return number;
 }
 
+/**
+ * input: указатель на начало участка памяти и указатель на конец участка
+ *
+ * output: отсортированный участок памяти
+ *
+ * Функция используя алгоритм порязрядной сортировки от маладших разрядов к старшим
+ * сортирует указанные массив целых чисел. В рамках определённых разрядак массив
+ * сортируется с помощью сортировки подсчётом.
+ * Данная функция, для избежания трудностей при сортировке отрицательных чисел,
+ * изменяет входной массив прибавляя к каждому элементу мниимальное значение типа int, 
+ * записывая новые значения в вспомогательный массив типа unsigned int
+ *
+ * @param *first - указатель на начало масива, на первый элемент
+ * @param *last - указатель на последний элемент массива
+ */
 void LSDSort(int* first, int* last)
 {
-	int length = (abs((char)first - (char)last))/4;
+	int length = (abs((char)first - (char)last))/4; //определяем количество элементов в массиве
 	length++;
-	unsigned int* buffer = new unsigned int[length];
+	unsigned int* buffer = new unsigned int[length]; 
 	unsigned int* first_u = new unsigned int[length];
 	for (int i = 0; i < length; i++)
 		first_u[i] = first[i] - std::numeric_limits<int>::min();
