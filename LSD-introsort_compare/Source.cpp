901fe3275c665bc06922ede3e2e823fae347fcd3
BLGALEX <lepehaleha@bk.ru>
2020-03-04 19:41:19 +1000

    Добавлен новый заголовочный файл  с интроспективной сортировкой для массива структур с двумя полями int . Сделано для проверки сортировки на устойчивость.

--- LSD-introsort_compare/Source.cpp
+++ LSD-introsort_compare/Source.cpp
@@ -4,44 +4,75 @@
 #include<conio.h>
 #include"Intro_sort.h"
 #include"LSD_sort.h"
+#include"Introsort_template.h"
+
+struct check_stable
+{
+	int key;
+	int check;
+};
+
+bool operator >(check_stable& a, check_stable& b)
+{
+	if (a.key > b.key)
+		return true;
+	return false;
+}
+
+bool operator <(check_stable& a, check_stable& b)
+{
+	if (a.key < b.key)
+		return true;
+	return false;
+}
+
+bool operator >=(check_stable& a, check_stable& b)
+{
+	if (a.key >= b.key)
+		return true;
+	return false;
+}
+
+bool operator <=(check_stable& a, check_stable& b)
+{
+	if (a.key <= b.key)
+		return true;
+	return false;
+}
+
+bool operator ==(check_stable& a, check_stable& b)
+{
+	if (a.key == b.key)
+		return true;
+	return false;
+}
+
+bool operator !=(check_stable& a, check_stable& b)
+{
+	if (a.key != b.key)
+		return true;
+	return false;
+}
+
+int comp(const void*a, const void* b)
+{
+	if (*(check_stable*)a < *(check_stable*)b) return -1;
+	if (*(check_stable*)a == *(check_stable*)b) return 0;
+	if (*(check_stable*)a > *(check_stable*)b) return 1;
+}
 
 int main()
 {
+	check_stable arr[10] = {2147000000, 1, 2147000000, 2, -2147000000, 3, 0, 4, 0, 5, -2147000000, 6, 1, 7, 7, 8, 9, 9, 5, 10};
 
-	int arr[10000], k, check = 0;
+	for (int i = 0; i < 10; i++)
+	{
+		arr[i].key = 0;
+		arr[i].check = i;
+	}
 
-	std::cout << "To enter your massive press 1 \n";
-	std::cout << "To set random massive whith 50 numbers press any key \n";
-	if (_getch() == '1')
-	{
-		unsigned short n = 0;
-		std::cout << "Enter massive size \n";
-		std::cin >> n;
-		std::cout << "Enter massive \n";
-		for (int i = 0; i < n; i++)
-			std::cin >> arr[i];
-		Introsort(arr, 0, n - 1);
-		for (int i = 0; i < n; i++)
-			std::cout << arr[i] << ' ';
-	}
-	else
-	{
-		srand(time(NULL));
-		for (int i = 0; i < 50; i++)
-		{
-			k = rand() % 2;
-			if (k == 1)
-				arr[i] = -rand() % 1000;
-			else
-				arr[i] = rand() % 1000;
-		}
-		for (int i = 0; i < 50; i++)
-			std::cout << arr[i] << ' ';
-		Introsort(arr, 0, 49);
-		std::cout << std::endl;
-		std::cout << std::endl;
-		for (int i = 0; i < 50; i++)
-			std::cout << arr[i] << ' ';
-	}
+	qsort(arr, 10, 8, comp);
+	Introsort_template(arr, 0, 9);
+
 	return 0;
-} //etg
+} 
